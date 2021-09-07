284c09984f2729bc1e7c98fcb5864d2228970629
slaurent <slaurent22@pm.me>
2021-09-07 09:28:31 -0400

    derive tree from data

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import tree from "./data.json";
 
 function indentStyle(indentLevel) {
   return {
@@ -6,16 +7,25 @@ function indentStyle(indentLevel) {
   }
 }
 
+// flatten to Array<{name: string; level:number;}>
+function flattenTree(tree, level=0) {
+  const { name, children } = tree;
+  const childElements =
+    children
+    .map(child => flattenTree(child, level + 1))
+    .flat();
+  return [ {name, level}, ...childElements ]
+}
+
+function nodeElement({name, level}) {
+  return <p style={indentStyle(level)}>{name}</p>;
+}
+
 export default function Tree() {
+  const treeElements = flattenTree(tree).map(nodeElement);
   return (
     <div className="tree">
-      <p style={indentStyle(0)}>root</p>
-      <p style={indentStyle(1)}>ant</p>
-      <p style={indentStyle(1)}>bear</p>
-      <p style={indentStyle(2)}>cat</p>
-      <p style={indentStyle(3)}>dog</p>
-      <p style={indentStyle(3)}>elephant</p>
-      <p style={indentStyle(1)}>frog</p>
+      {treeElements}
     </div>
   );
 }
