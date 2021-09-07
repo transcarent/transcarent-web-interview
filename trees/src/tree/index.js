d2163a49f4366bbe1266a8665b4c9a530d89804d
slaurent <slaurent22@pm.me>
2021-09-07 10:00:06 -0400

    update name: get working

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,5 +1,6 @@
 import "./index.css";
-import tree from "./data.json";
+import data from "./data.json";
+import React from "react";
 
 function indentStyle(indentLevel) {
   return {
@@ -17,15 +18,60 @@ function flattenTree(tree, level=0) {
   return [ {name, level}, ...childElements ]
 }
 
-function nodeElement({name, level}) {
-  return <p key={name} style={indentStyle(level)}>{name}</p>;
+function cloneTree({name, children}) {
+  const childClones = children.map(cloneTree);
+  return Object.assign({}, {
+    name, children: childClones
+  });
 }
 
-export default function Tree() {
-  const treeElements = flattenTree(tree).map(nodeElement);
-  return (
-    <div className="tree">
-      {treeElements}
-    </div>
-  );
+function findElement(tree, name) {
+  if (tree.name === name) {
+    return tree;
+  }
+  for (const child of tree.children) {
+    const found = findElement(child, name);
+    if (found) {
+      return found;
+    }
+  }
+  return null;
+}
+
+export default class Tree extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tree: data };
+  }
+  render() {
+    console.log("render");
+    const treeElements =
+      flattenTree(this.state.tree)
+      .map(this.nodeElement.bind(this));
+    return (
+      <div className="tree">
+        {treeElements}
+      </div>
+    );
+  }
+  updateElement(name, event) {
+    event.preventDefault();
+    const newTree = cloneTree(this.state.tree);
+    const nodeToUpdate = findElement(newTree, name);
+    nodeToUpdate.name = event.target[0].value;
+    this.setState({tree: newTree});
+  }
+  nodeElement({name, level}) {
+    return (
+      <div key={name}>
+        <p style={indentStyle(level)} >
+          {name}
+        </p>
+        <form onSubmit={this.updateElement.bind(this, name)}>
+          <input type="text" style={indentStyle(level)} />
+        </form>
+      </div>
+    );
+  }
+
 }
