de68c1b767848fcf5b08546b8a65590474d06e46
slaurent <slaurent22@pm.me>
2021-09-07 09:11:35 -0400

    use parameterized text-indent

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,15 +1,21 @@
 import "./index.css";
 
+function indentStyle(indentLevel) {
+  return {
+    "text-indent": `${4*indentLevel}ch`
+  }
+}
+
 export default function Tree() {
   return (
     <div className="tree">
-      <p>root</p>
-      <p className="indent1">ant</p>
-      <p className="indent1">bear</p>
-      <p className="indent2">cat</p>
-      <p className="indent3">dog</p>
-      <p className="indent3">elephant</p>
-      <p className="indent1">frog</p>
+      <p style={indentStyle(0)}>root</p>
+      <p style={indentStyle(1)}>ant</p>
+      <p style={indentStyle(1)}>bear</p>
+      <p style={indentStyle(2)}>cat</p>
+      <p style={indentStyle(3)}>dog</p>
+      <p style={indentStyle(3)}>elephant</p>
+      <p style={indentStyle(1)}>frog</p>
     </div>
   );
 }
