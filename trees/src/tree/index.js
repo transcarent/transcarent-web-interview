6d8207dc4102746348d9d320b701863027ebdaed
Patrick Holloway <patrickjholloway@gmail.com>
2021-11-28 12:55:45 -0500

    confirm results are the same

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -12,6 +12,17 @@ function TreeNode({ value, children }) {
 export default function Tree() {
   return (
     <div className="tree">
+      <div className="old-tree">
+        <p>root</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
+        </p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+      </div>
       <TreeNode value="root">
         <TreeNode value="ant" />
         <TreeNode value="bear">
