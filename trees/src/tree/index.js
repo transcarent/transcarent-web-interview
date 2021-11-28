0212f0924a01928c6d9004af07758df458a62ce1
Patrick Holloway <patrickjholloway@gmail.com>
2021-11-28 12:46:23 -0500

    replace inline &nbsp; styling

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,17 +1,27 @@
 import "./index.css";
 
+function TreeNode({ value, children }) {
+  return (
+    <div class="node">
+      <div class="value">{value}</div>
+      <div class="children">{children}</div>
+    </div>
+  );
+}
+
 export default function Tree() {
   return (
     <div className="tree">
-      <p>root</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
-      <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
-      </p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+      <TreeNode value="root">
+        <TreeNode value="ant" />
+        <TreeNode value="bear">
+          <TreeNode value="cat"></TreeNode>
+          <TreeNode value="dog">
+            <TreeNode value="elephant"></TreeNode>
+          </TreeNode>
+        </TreeNode>
+        <TreeNode value="frog" />
+      </TreeNode>
     </div>
   );
 }
